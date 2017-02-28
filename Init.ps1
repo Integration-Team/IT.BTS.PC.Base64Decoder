7cf57863c774fde0f93d574ec13204ecb1f9192d
Stefan Van Camp <stefan.vancamp@kapsch.net>
2017-02-28 18:20:38 +0100

    Updated script

--- Init.ps1
+++ Init.ps1
@@ -1,2 +1,8 @@
-﻿param($package)
-copy "lib\net45\" + $package + ".dll" "C:\Program Files (x86)\Microsoft BizTalk Server 2013 R2\Pipeline Components\" + $package + ".dll"
+﻿$pcPath = "C:\Program Files (x86)\Microsoft BizTalk Server 2013 R2\Pipeline Components\"
+if (Test-Path $pcPath){
+    $currentLocation = Get-Location
+    xcopy  $currentLocation"\lib\net45\IT.BTS.PC.Base64Decoder.dll" "C:\Program Files (x86)\Microsoft BizTalk Server 2013 R2\Pipeline Components\"
+}
+else{
+    Write-Output "Pipeline component path not found"
+}
