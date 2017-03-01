bbabf5521960a2744bb93b9716c83dfbcf660cca
Stefan Van Camp <stefan.vancamp@kapsch.net>
2017-03-01 13:12:33 +0100

    Updated nuspec and Init script

--- Init.ps1
+++ Init.ps1
@@ -1,7 +1,9 @@
 ﻿$pcPath = "C:\Program Files (x86)\Microsoft BizTalk Server 2013 R2\Pipeline Components\"
 if (Test-Path $pcPath){
     $currentLocation = Get-Location
-    xcopy  $currentLocation"\lib\net45\IT.BTS.PC.Base64Decoder.dll" "C:\Program Files (x86)\Microsoft BizTalk Server 2013 R2\Pipeline Components\"
+    $sourceFile = "$currentLocation\lib\net45\IT.BTS.PC.Base64Decoder.dll"
+    $targetLocation = "C:\Program Files (x86)\Microsoft BizTalk Server 2013 R2\Pipeline Components\"
+    xcopy $sourceFile $targetLocation /y
 }
 else{
     Write-Output "Pipeline component path not found"
