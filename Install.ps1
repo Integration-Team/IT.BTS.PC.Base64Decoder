0c3b8bc5b2e53f1c8a40b9ab0706e87a52500976
Stefan Van Camp <stefan.vancamp@kapsch.net>
2017-03-02 16:44:39 +0100

    Deploy to GAC on install package works

---
+++ Install.ps1
@@ -0,0 +1,12 @@
+﻿param(
+    [Parameter(Mandatory=$true)] [string]   $installPath,
+    [Parameter(Mandatory=$true)] [string]   $toolsPath,
+    [Parameter(Mandatory=$true)]            $package,
+    [Parameter(Mandatory=$true)]            $project
+)
+
+$assemblyName = "IT.BTS.PC.Base64Decoder"
+
+$packageRoot = $(Get-Item $toolsPath).Parent.FullName
+$sourceFile = "$packageRoot\lib\net45\$assemblyName.dll"
+gacutil /if $sourcefile
